1aa9d35658c6d34387b95bda8dda55577a4b8fa3
bedreski <fabianabedreski@gmail.com>
2023-02-02 16:41:08 -0300

    delete handle length message and born_at validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,8 @@ class User < ApplicationRecord
   validates :handle, presence: true, #"check if the value is either nil or a blank string, that is, a string that is either empty or consists of whitespace" [Rails Guides]
             length: {
               minimum: 4, 
-              :too_short: "must have at least %{count} characters", 
-              maximum: 12, "%{count} characters is the maximum allowed"
-              :too_long: 
-            }
+              maximum: 12,
+            },
             uniqueness: true, 
             format: {
               with: /\A[\w]+\z/,
@@ -21,10 +19,6 @@ class User < ApplicationRecord
   
   validates :bio, length: {maximum: 300}
 
-  validates :born_at, presence: true, 
-            numericality: { 
-              less_than_or_equal_to: Date.current - 13, 
-              message: "must be at least %{count}"
-            }
+  validates :born_at, presence: true
 
 end
