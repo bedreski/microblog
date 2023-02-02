ac445512e6aa88dc93ab3b725b94c54b48dcfb30
bedreski <fabianabedreski@gmail.com>
2023-02-02 16:44:07 -0300

    add validations for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,48 @@ require_relative '../spec_helper.rb'
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { build(:user) }
 
-  
+  before { FactoryBot.build(:user) }
+
+  describe 'handle' do
+    context 'is not empty' do
+      it { is_expected.to validate_presence_of(:handle).on(:create) }
+    end 
+
+    context 'have length inside of range' do
+      it { is_expected.to validate_length_of(:handle).is_at_least(4).is_at_most(12) }
+    end 
+
+    context 'is in the correct pattern' do 
+      it { is_expected.to allow_value('som_eName').for(:handle) }
+    end 
+  end 
+
+  describe 'display name' do
+
+    context 'is not empty' do
+      it { is_expected.to validate_presence_of(:display_name) }
+    end
+
+    context 'respect maximum characters length' do 
+      it { is_expected.to validate_length_of(:display_name).is_at_least(1).is_at_most(30) }
+    end 
+  end 
+
+  describe 'bio' do 
+
+    context 'when is supplied' do 
+      it { is_expected.to validate_length_of(:bio).is_at_most(300) }
+    end 
+  end 
+
+  describe 'age' do 
+    context 'is not empty' do 
+      it { is_expected.to validate_presence_of(:born_at) }
+    end 
+
+    context 'is greater than or equal to 13' do
+      # it { }
+    end 
+  end 
 end
