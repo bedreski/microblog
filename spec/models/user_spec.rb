80491e01643a42f0269970cb3c33dc5464d4de57
bedreski <fabianabedreski@gmail.com>
2023-02-02 01:29:06 -0300

    change to FactoryBot test example

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,22 +1,8 @@
 require_relative '../spec_helper.rb'
 require 'rails_helper'
 
-# RSpec.describe User, type: :model do
-#   let(:user) { build(:user) }
-# end
+RSpec.describe User, type: :model do
+  let(:user) { build(:user) }
 
-describe User do 
-
-  let(:user) {User.new}
-
-  describe '#username' do
-
-    context 'when handle is not provided' do
-
-      it 'does not let user proceed' do
-        expect()
-    end 
-  end 
-  #then use case 
-
-end 
+  
+end
