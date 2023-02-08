9c03a49b6d56154ab360a92b579ea11d2993db91
bedreski <fabianabedreski@gmail.com>
2023-02-08 09:02:27 -0300

    start tests for media

---
+++ spec/models/medium_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Medium, type: :model do
+  
+  before { build :medium }
+
+  describe 'media type' do 
+
+    context 'when is supported' do 
+      #can I call medium model method here to test?
+    end 
+  end 
+end
