86509bf05d8b35738a085896fef82047a0468b74
bedreski <fabianabedreski@gmail.com>
2023-02-08 09:16:44 -0300

    add trait and 'question' comment

--- spec/factories/media.rb
+++ spec/factories/media.rb
@@ -3,5 +3,13 @@ FactoryBot.define do
     kind { 1 }
     url { "https://wallpaperaccess.com/full/2971852.jpg" }
     status { 1 }
+    create { :status } #media needs a status
+
+    trait :url_is_secure do 
+      URI.parse(url).scheme = 'https' 
+    end 
+
+    #can I call model method here inside a 'trait' (for 'kind' attribute test)?
+
   end
 end
