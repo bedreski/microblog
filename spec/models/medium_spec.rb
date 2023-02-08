e5a44946ba739560bb475785778f0b6be14307d2
bedreski <fabianabedreski@gmail.com>
2023-02-08 09:17:12 -0300

    start tests for medium model

--- spec/models/medium_spec.rb
+++ spec/models/medium_spec.rb
@@ -7,7 +7,15 @@ RSpec.describe Medium, type: :model do
   describe 'media type' do 
 
     context 'when is supported' do 
-      #can I call medium model method here to test?
+      
     end 
+  end 
+
+  describe 'URL' do 
+
+    context 'when is secure' do 
+      #test if url scheme is 'https'
+    end 
+
   end 
 end
