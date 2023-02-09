5e048aedf1297d1828a6a9756ecc36fe7c9b9ccd
bedreski <fabianabedreski@gmail.com>
2023-02-09 05:04:53 -0300

    just one line

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_02_08_030416) do
-
   create_table "follows", force: :cascade do |t|
     t.integer "follower_id", null: false
     t.integer "followed_id", null: false
