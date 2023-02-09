378e0eafd3da0fd0456b4a95ec60bf8b1d7ce420
bedreski <fabianabedreski@gmail.com>
2023-02-09 05:03:24 -0300

    create first routes to index/dashboard/statuses page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "statuses#index"
+
+  get '/statuses', to: 'statuses#index'
+  post '/statuses', to: 'statuses#create' #call create method 
 end
