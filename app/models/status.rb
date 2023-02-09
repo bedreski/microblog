e6488489deda779f83eea07a42674a8ee63a8080
bedreski <fabianabedreski@gmail.com>
2023-02-09 02:17:25 -0300

    typo in length validation

--- app/models/status.rb
+++ app/models/status.rb
@@ -3,5 +3,5 @@ class Status < ApplicationRecord
   has_many :media 
 
   validates :body, presence:true, length: { maximum: 300 }
-  validates :media, length { maximum: 4, message: "Status can have a maximum of 4 media"}
+  validates :media, length: { maximum: 4, message: "Status can have a maximum of 4 media" }
 end
